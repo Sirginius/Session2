6b7f7e2eded56a059ff5b4536bd555246fc49bbb
User <User@DESKTOP-0LJAE9P>
2020-12-15 12:35:32 +0500

    Second commit

--- OnlineStudy/Properties/Resources.Designer.cs
+++ OnlineStudy/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace OnlineStudy.Properties {
                 return ((System.Drawing.Icon)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap school_logo1 {
+            get {
+                object obj = ResourceManager.GetObject("school_logo1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
